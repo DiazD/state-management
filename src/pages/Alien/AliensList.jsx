be844287edbf3d1030413448d3db32727f22c731
Diego Diaz <ddiaz@admins-MacBook-Pro.local>
2022-11-02 20:42:50 -0700

    update state renderer to have a live view of the data

--- src/pages/Alien/AliensList.jsx
+++ src/pages/Alien/AliensList.jsx
@@ -1,109 +1,111 @@
-import { useRef, memo } from "react";
+import { useRef, memo, useEffect } from "react";
 import { useSubscription, dispatch } from "../../jekyll";
 import "./Alien.css";
 
 const Checkbox = ({ id }) => {
-  const isChecked = useSubscription(["ui", "aliens", "selections"], (selections) => selections.includes(id))
-  const onChange = () => {
-    dispatch(["ui-select-alien", { id: id, add: !isChecked }])
-  }
-  return <input type="checkbox" checked={isChecked} onChange={onChange} />;
+    const isChecked = useSubscription(["ui", "aliens", "selections"], (selections) => selections.includes(id))
+    const onChange = () => {
+        dispatch(["ui-select-alien", { id: id, add: !isChecked }])
+    }
+    return <input type="checkbox" checked={isChecked} onChange={onChange} />;
 };
 
 const AlienDetails = ({ alien }) => (
-  <>
-    <span className="card-column">{alien.first_name}</span>
-    <span className="card-column">{alien.last_name}</span>
-    <span className="card-column">{alien.email}</span>
-    <span className="card-column">{alien.age}</span>
-    <span className="card-column text-left">{alien.favorite_animal}</span>
-    <span className="card-column">
-      <button onClick={() => dispatch(["ui-set-alien-to-edit", alien.id])}>edit</button>
-      <button onClick={() => dispatch(["ui-delete-alien", alien.id])}>delete</button>
-      <button onClick={() => dispatch(["ui-set-alien-to-view", alien.id])}>view</button>
-    </span>
-  </>
+    <>
+        <span className="card-column">{alien.first_name}</span>
+        <span className="card-column">{alien.last_name}</span>
+        <span className="card-column">{alien.email}</span>
+        <span className="card-column">{alien.age}</span>
+        <span className="card-column text-left">{alien.favorite_animal}</span>
+        <span className="card-column">
+            <button onClick={() => dispatch(["ui-set-alien-to-edit", alien.id])}>edit</button>
+            <button onClick={() => dispatch(["ui-delete-alien", alien.id])}>delete</button>
+            <button onClick={() => dispatch(["ui-set-alien-to-view", alien.id])}>view</button>
+        </span>
+    </>
 );
 
 const AlienForm = ({ alien }) => {
-  const firstNameRef = useRef(alien.first_name);
-  const lastNameRef = useRef(alien.last_name);
-  const emailRef = useRef(alien.email);
-  const ageRef = useRef(alien.age);
-  const favoriteAnimalRef = useRef(alien.favorite_animal);
+    const firstNameRef = useRef(alien.first_name);
+    const lastNameRef = useRef(alien.last_name);
+    const emailRef = useRef(alien.email);
+    const ageRef = useRef(alien.age);
+    const favoriteAnimalRef = useRef(alien.favorite_animal);
 
-  const onSubmit = () => {
-    const payload = {
-      first_name: firstNameRef.current.value,
-      last_name: lastNameRef.current.value,
-      email: emailRef.current.value,
-      age: parseInt(ageRef.current.value),
-      favorite_animal: favoriteAnimalRef.current.value,
+    const onSubmit = () => {
+        const payload = {
+            first_name: firstNameRef.current.value,
+            last_name: lastNameRef.current.value,
+            email: emailRef.current.value,
+            age: parseInt(ageRef.current.value),
+            favorite_animal: favoriteAnimalRef.current.value,
+        };
+
+        dispatch(["ui-update-alien-info", { ...alien, ...payload }]);
     };
 
-    dispatch(["ui-update-alien-info", { ...alien, ...payload }]);
-  };
+    const cancelEdit = () => dispatch(["ui-edit-alien/cancel-edit", alien.id]);
 
-  const cancelEdit = () => dispatch(["ui-edit-alien/cancel-edit", alien.id]);
-
-  return (
-    <>
-      <span className="card-column">
-        <input ref={firstNameRef} name="first_name" defaultValue={alien.first_name} />
-      </span>
-      <span className="card-column">
-        <input ref={lastNameRef} name="last_name" defaultValue={alien.last_name} />
-      </span>
-      <span className="card-column">
-        <input ref={emailRef} name="email" defaultValue={alien.email} />
-      </span>
-      <span className="card-column">
-        <input ref={ageRef} name="age" defaultValue={alien.age} />
-      </span>
-      <span className="card-column text-left">
-        <input ref={favoriteAnimalRef} name="favorite_animal" defaultValue={alien.favorite_animal} />
-      </span>
-      <span className="card-column">
-        <button onClick={onSubmit}>save</button>
-        <button onClick={cancelEdit}>cancel</button>
-      </span>
-    </>
-  )
+    return (
+        <>
+            <span className="card-column">
+                <input ref={firstNameRef} name="first_name" defaultValue={alien.first_name} />
+            </span>
+            <span className="card-column">
+                <input ref={lastNameRef} name="last_name" defaultValue={alien.last_name} />
+            </span>
+            <span className="card-column">
+                <input ref={emailRef} name="email" defaultValue={alien.email} />
+            </span>
+            <span className="card-column">
+                <input ref={ageRef} name="age" defaultValue={alien.age} />
+            </span>
+            <span className="card-column text-left">
+                <input ref={favoriteAnimalRef} name="favorite_animal" defaultValue={alien.favorite_animal} />
+            </span>
+            <span className="card-column">
+                <button onClick={onSubmit}>save</button>
+                <button onClick={cancelEdit}>cancel</button>
+            </span>
+        </>
+    )
 };
 
-const AlienEditableDetails = ({ alien }) => {
-  const isEditing = useSubscription(
-    ["ui", "aliens", "aliensBeingEdited"],
-    (aliensBeingEdited) => aliensBeingEdited.includes(alien.id)
-  );
-  return isEditing ? <AlienForm alien={alien} /> : <AlienDetails alien={alien} isEditing={isEditing} />;
+const AlienEditableDetails = ({ id }) => {
+    const [alien] = useSubscription(["aliens", id])
+    const isEditing = useSubscription(
+        ["ui", "aliens", "aliensBeingEdited"],
+        (aliensBeingEdited) => aliensBeingEdited.includes(alien.id)
+    );
+    return isEditing ? <AlienForm alien={alien} /> : <AlienDetails alien={alien} isEditing={isEditing} />;
 };
 
 const Alien_ = ({ id }) => {
-  const alien = useSubscription(["aliens", id], (alien) => alien);
-  return (
-    <div id={`alien-${id}`} style={{ display: "flex", alignItems: "center" }}>
-      <Checkbox id={id} />
-      <div className="card">
-        <span className="card-column text-left"><img alt="hello" src={alien.image} /></span>
-        <AlienEditableDetails alien={alien} />
-      </div>
-    </div>
-  );
+    const image = useSubscription(['aliens', id, 'image']);
+
+    return (
+        <div id={`alien-${id}`} style={{ display: "flex", alignItems: "center" }}>
+            <Checkbox id={id} />
+            <div className="card">
+                <span className="card-column text-left"><img alt="hello" src={image} /></span>
+                <AlienEditableDetails id={id} />
+            </div>
+        </div>
+    );
 }
 const Alien = memo(Alien_); // memoize component to avoid re-rendering on `deletion`
 
 const Aliens = () => {
-  const aliens = useSubscription(
-    ["ui", "aliens", "aliensToShow"],
-    (aliens) => aliens
-  );
+    const aliens = useSubscription(
+        ["ui", "aliens", "aliensToShow"],
+        (aliens) => aliens
+    );
 
-  return (
-    <div className="card-container">
-      {aliens.map((id) => <Alien key={id} id={id} />)}
-    </div>
-  );
+    return (
+        <div className="card-container">
+            {aliens.map((id) => <Alien key={id} id={id} />)}
+        </div>
+    );
 };
 
 export default Aliens;
--- src/pages/State/events.js
+++ src/pages/State/events.js
@@ -1,24 +1,25 @@
 import { registerEvent } from "../../jekyll";
 
 registerEvent({
-  event: "ui-state-set-paths-to-view",
-  paths: [["ui", "state", "viewedPaths"]],
-  handler: ({ viewedPaths }, result) => ({
-    state: [
-      {
-        path: ["ui", "state", "viewedPaths"],
-        data: [...viewedPaths, { ...result, value: result.value.split(",") }]
-      },
-    ],
-  })
+    event: "ui-state-set-paths-to-view",
+    paths: [["ui", "state", "viewedPaths"]],
+    handler: ({ viewedPaths }, result) => ({
+        state: [
+            {
+                path: ["ui", "state", "viewedPaths"],
+                data: [...viewedPaths, { ...result, value: result.value.split(",") }]
+            },
+        ],
+    })
 });
 
 registerEvent({
-  event: "ui-state-show-tree",
-  paths: [["ui", "state", "showStateTree"]],
-  handler: ({ showStateTree }, data) => ({
-    state: [
-      { path: ["ui", "state", "showStateTree"], data }
-    ]
-  })
+    event: "ui-state-show-tree",
+    // NOTE: I don't think I need the path....
+    paths: [["ui", "state", "showStateTree"]],
+    handler: ({ showStateTree }, data) => ({
+        state: [
+            { path: ["ui", "state", "showStateTree"], data }
+        ]
+    })
 });
